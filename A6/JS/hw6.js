d0f46034c47c77c6e80053e68bcd135432c53178
kimste2 <steven_kim@student.uml.edu>
2018-11-11 14:06:54 -0500

    functioning table, make pretty

--- A6/JS/hw6.js
+++ A6/JS/hw6.js
@@ -3,17 +3,22 @@
 // COMP 4610: Assignment 6
 // Date: 11.1.18
 // Description: makes a multiplication table from user input
-
+var end_limit = 50;
+var start_limit = 0;
+var h_start, h_end, v_start, v_end;
 function mult_table()
 {
 	// get local variables from HTML
-	console.log("Mult table function");
-	var h_start, h_end, v_start, v_end;
+//	console.log("Mult table function");
+
 	h_start = document.getElementById("horizontalStart").value;
-	check_input_lower(h_start, "horizontalStart");
+	check_input_lower(h_start, "horizontal starting number ");
 	h_end = document.getElementById("horizontalEnd").value;
+	check_input_upper(h_end, "Horizontal end ", h_start);
 	v_start = document.getElementById("verticalStart").value;
+	check_input_lower(v_start, "Vertical starting number ");
 	v_end = document.getElementById("verticalEnd").value;
+	check_input_upper(v_end, "Vertical ending number ", v_start);
 	//console.log("h_start has " + h_start);
 	//console.log("h_end has " + h_end);
 	//console.log("v_start has " + v_start);
@@ -24,7 +29,10 @@ function mult_table()
 	// make a table
 	var i, j;
  	table += "<tr>\n"
+
+	// intentionally left blank
 	table += "<td></td>"
+
 	for(j = h_start; j <= h_end; ++j)
 	{
 		table += "<td>" + j + "</td>\n";
@@ -48,20 +56,32 @@ function mult_table()
 
 }
 
+// Parameters for check_input_lower:
+	// p1 is the number that is being checked
+	// p2 is a string name for the id for p1
 function check_input_lower(p1, p2)
 {
-		var end_limit = 50;
-		var start_limit = 0;
-
 		if(isNaN(p1) || p1 < start_limit || p1 > end_limit)
 		{
-		  alert(p2 + " must be a number between " + start_limit + " and " + end_limit);
+			alert(p2 + "must be a number between " + start_limit + " and " + end_limit + ".");
+			var error = "Restart webpage";
+			document.getElementById("error").innerHTML = error;
 		}
 
 } // end of check_input_lower funciton
 
-function check_input_upper()
-{
 
+// Parameters for check_input_lower:
+	// p1 is the number that is being checked
+	// p2 is a string name for the id for p1
+	// p3 is the lower limit
+function check_input_upper(p1, p2 , p3)
+{
+	if(isNaN(p1) ||  p1 < start_limit || p1 > end_limit || p1 < p3)
+	{
+		alert(p2 + "must be a number between " + start_limit + " and " + end_limit + " as well as be larger than " + p3 + ".");
+		var error = "Restart webpage";
+		document.getElementById("error").innerHTML = error;
+	}
 
 } // end of check_input_upper function
