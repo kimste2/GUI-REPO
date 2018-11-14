57e12b41b0707f85a4c7b28fb55717a9824a614a
kimste2 <steven_kim@student.uml.edu>
2018-11-14 12:44:35 -0500

    CSS color change

--- A6/JS/hw6.js
+++ A6/JS/hw6.js
@@ -64,8 +64,8 @@ function check_input_lower(p1, p2)
 		if(isNaN(p1) || p1 < start_limit || p1 > end_limit)
 		{
 			alert(p2 + "must be a number between " + start_limit + " and " + end_limit + ".");
-			var error = "Restart webpage";
-			document.getElementById("error").innerHTML = error;
+		//	var error = "Restart webpage";
+		//	document.getElementById("error").innerHTML = error;
 		}
 
 } // end of check_input_lower funciton
@@ -80,8 +80,8 @@ function check_input_upper(p1, p2 , p3)
 	if(isNaN(p1) ||  p1 < start_limit || p1 > end_limit || p1 < p3)
 	{
 		alert(p2 + "must be a number between " + start_limit + " and " + end_limit + " as well as be larger than " + p3 + ".");
-		var error = "Restart webpage";
-		document.getElementById("error").innerHTML = error;
+	//	var error = "Restart webpage";
+	//	document.getElementById("error").innerHTML = error;
 	}
 
 } // end of check_input_upper function
