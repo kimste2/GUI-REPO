bf61ee722f21ae134aa59609f271b7cf394decfd
kimste2 <steven_kim@student.uml.edu>
2018-11-24 10:54:17 -0500

    revised some validation rules in js file

--- A7/JS/hw7.js
+++ A7/JS/hw7.js
@@ -10,29 +10,29 @@ var h_start, h_end, v_start, v_end;
 function mult_table()
 {
 	// get local variables from HTML
-	console.log("Mult table function");
+	//console.log("Mult table function");
   h_start = document.getElementById("horizontalStart").value;
 	h_end = document.getElementById("horizontalEnd").value;
 	v_start = document.getElementById("verticalStart").value;
 	v_end = document.getElementById("verticalEnd").value;
 
 	if(parseInt(h_start) > parseInt(h_end)){
-		alert("error");
+		//alert("error");
 		var temp = h_start;
 		h_start = h_end;
 		h_end = temp;
 	}
 
 	if(parseInt(v_start) > parseInt(v_end)){
-		alert("error");
+	//	alert("error");
 		var temp = v_start;
 		v_start = v_end;
 		v_end = temp;
 	}
-	console.log("h_start has " + h_start);
-	console.log("h_end has " + h_end);
-	console.log("v_start has " + v_start);
-	console.log("v_end has " + v_end);
+	//console.log("h_start has " + h_start);
+	//console.log("h_end has " + h_end);
+	//console.log("v_start has " + v_start);
+	//console.log("v_end has " + v_end);
 
 	var table = "<table class=\"mult_table\">\n<tbody>\n"
 	//console.log(table);
@@ -77,17 +77,35 @@ var validtor = $("#numbers").validate({
 				max: 50,
 				number: true
 			},
-
+			horizontalStart:{
+				min: 0,
+				max: "#horizontalEnd",
+			},
+			horizontalEnd:{
+				min: "#horizontalStart",
+				max: 50,
+			},
+			verticalStart:{
+				min: 0,
+				max: "#verticalEnd",
+			},
+			verticalEnd:{
+				min: "#verticalStart",
+				max: 50,
+			}
 		}, // end of rules
-		messages: {
-			number: "This is a required field, please ensure you number is between 0 and 50."
-		}
+		messages:{
+			horizontalStart: "Please enter a number less than the (horizontal) ending number.",
+			horizontalEnd: "Please enter a number greater than the (horizontal) starting number.",
+			veriticalStart:"Please enter a number less than the (vertical) ending number.",
+			verticalEnd: "Please enter a number greater than the (vertical) starting number. ",
+		},
 
 }); // end of validate
 
 
 $('#button').on('click', function() {
-	$("#numbers").valid();
-	alert("Do you have what you want?\n" + $("#horizontalStart").val() + " : " + $("#horizontalEnd").val() + "\n" + $("#verticalStart").val() + " : " + $("#verticalEnd").val());
+	$("#numbers").validate();
+	//alert("Do you have what you want?\n" + $("#horizontalStart").val() + " : " + $("#horizontalEnd").val() + "\n" + $("#verticalStart").val() + " : " + $("#verticalEnd").val());
 	mult_table();
 });
